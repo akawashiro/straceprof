5aa2c54556818d058dc7eceed90b20233f2187b0
Akira Kawata <akirakawata1@gmail.com>
2025-03-16 21:30:39 +0900

    Replace Vite + React in straceprof-js directory because the name of application is straceprof

--- straceprof-js/src/App.tsx
+++ straceprof-js/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState, useRef } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
@@ -30,15 +28,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>straceprof</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -71,9 +61,7 @@ function App() {
           </div>
         )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">straceprof - A profiling tool for strace</p>
     </>
   );
 }
