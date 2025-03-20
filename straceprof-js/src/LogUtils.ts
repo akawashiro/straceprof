3407a3d16b4cd45907314934dc86e78b927fbf92
Akira Kawata <akirakawata1@gmail.com>
2025-03-20 17:17:16 +0900

    Fix path

--- straceprof-js/src/LogUtils.ts
+++ straceprof-js/src/LogUtils.ts
@@ -8,7 +8,7 @@
  * @returns The content of the log file as a string
  */
 export async function fetchLog(logName: string): Promise<string> {
-  const response = await fetch(`/${logName}`);
+  const response = await fetch(`/straceprof/${logName}`);
   if (!response.ok) {
     throw new Error(`Failed to fetch log: ${logName}`);
   }
